9dbee4e8cba35a5905673885cdcbf5a4ff6c35c6
AntonKimfors <antonkimfors@hotmail.com>
2018-10-19 22:24:34 +0200

    Updated from dummy data to using Yelp API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp.js'
 
-let business = {
+/*let business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
@@ -15,23 +15,32 @@ let business = {
   category: 'Italian',
   rating: 4.5,
   reviewCount: 90
-}
+}*/
 
-const businesses = [business, business, business, business, business, business];
+//const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
-  searchYelp(term, location, sortBy){
-    console.log("Searching Yelp with ${term}, ${location}, ${sortBy}")
+  constructor(){
+    super();
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
 
+  searchYelp(term, location, sortBy){
+      Yelp.search(term,location,sortBy).then(businesses => {
+        this.setState({businesses: businesses})
+      });
   }
 
 
   render() {
     return (
       <div className="App">
-  <h1>ravenous</h1>
+  <h1>Matvrak?</h1>
     <SearchBar searchYelp={this.searchYelp}/>
-    <BusinessList businesses={businesses} />
+    <BusinessList businesses={this.state.businesses} />
 </div>
     );
   }
